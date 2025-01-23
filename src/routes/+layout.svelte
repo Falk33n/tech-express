8ffb27adf8a9591ccde248acc0f598feebc5cc09
Falk33n <tim.falk00@gmail.com>
2025-01-23 19:57:25 +0100

    feat: add layout components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,16 @@
 <script lang="ts">
+	import { Footer, Navbar } from '$lib/components/layout';
+	import { Sonner } from '$lib/components/ui/sonner';
 	import '$src/app.css';
+	import { ModeWatcher } from 'mode-watcher';
+
 	let { children } = $props();
 </script>
 
-{@render children()}
+<ModeWatcher />
+<Sonner />
+<div class="min-h-screen w-full bg-background text-foreground">
+	<Navbar />
+	<main id="main">{@render children()}</main>
+	<Footer />
+</div>
