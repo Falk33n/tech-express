22a4d1020aca2406663ab3eaaac7f477dd33eabd
Falk33n <tim.falk00@gmail.com>
2025-01-23 19:58:35 +0100

    feat: add shadcn sheet

---
+++ src/lib/components/ui/sheet/SheetDescription.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { SheetPrimitiveDescription } from '$lib/components/ui/sheet';
+	import { cn } from '$lib/utils';
+	import { type DialogDescriptionProps as SheetPrimitiveDescriptionProps } from 'bits-ui';
+
+	let {
+		ref = $bindable(null),
+		class: className,
+		...props
+	}: SheetPrimitiveDescriptionProps = $props();
+</script>
+
+<SheetPrimitiveDescription
+	bind:ref
+	class={cn('text-sm text-muted-foreground', className)}
+	{...props}
+/>
---
+++ src/lib/components/ui/sheet/SheetTitle.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { SheetPrimitiveTitle } from '$lib/components/ui/sheet';
+	import { cn } from '$lib/utils.js';
+	import { type DialogTitleProps as SheetPrimitiveTitleProps } from 'bits-ui';
+
+	let {
+		ref = $bindable(null),
+		class: className,
+		...props
+	}: SheetPrimitiveTitleProps = $props();
+</script>
+
+<SheetPrimitiveTitle
+	bind:ref
+	class={cn('text-lg font-semibold text-foreground', className)}
+	{...props}
+/>
