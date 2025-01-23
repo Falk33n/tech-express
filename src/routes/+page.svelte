e60f874ab97a7ea4304737e08b2c4f7fe8f17581
Falk33n <tim.falk00@gmail.com>
2025-01-23 19:57:02 +0100

    feat: add load function to get products homepage

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import {
+		Categories,
+		Hero,
+		LatestProducts,
+		Newsletter,
+		PopularProducts,
+	} from '$lib/components/home';
+	import type { PageProps } from './$types';
+
+	let { data }: PageProps = $props();
+
+	const isProductsEmpty = data.products.length < 3;
+	const latestThreeProducts = !isProductsEmpty ? data.products.slice(0, 3) : [];
+</script>
+
+<Hero />
+<PopularProducts />
+<Categories />
+<LatestProducts {latestThreeProducts} />
+<Newsletter />
