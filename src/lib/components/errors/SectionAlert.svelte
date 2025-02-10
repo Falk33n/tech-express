5cce63c99834e00ba7ed81b30515cd002ed81a50
Falk33n <tim.falk00@gmail.com>
2025-02-10 06:59:16 +0100

    feat: add error specific components

---
+++ src/lib/components/errors/SectionAlert.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import {
+		Alert,
+		AlertDescription,
+		AlertTitle,
+	} from '$lib/components/ui/alert';
+	import type { Snippet } from 'svelte';
+
+	type Props = { children?: Snippet<[]> };
+
+	let { children }: Props = $props();
+</script>
+
+<Alert
+	variant="destructive"
+	class="mx-auto w-[calc(100%-2rem)] sm:w-[70%] sm:max-w-[40rem]"
+>
+	<AlertTitle class="text-lg font-semibold">Något gick fel!</AlertTitle>
+	<AlertDescription>
+		{@render children?.()}
+	</AlertDescription>
+</Alert>
