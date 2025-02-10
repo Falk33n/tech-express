b893c165ae5c06228d2da0b13bbff5e0f2236ba8
Falk33n <tim.falk00@gmail.com>
2025-02-10 07:09:29 +0100

    temp: add button to mock add data

---
+++ src/routes/products/[id]/+page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	let amount = $state(0);
+	async function onclick() {
+		const mockdata = {
+			name: 'Testing',
+			description: 'Lorem ipsum dolar amet',
+			imageDescription: 'Lorem ipsum dolar amet',
+			imageUrl: 'https://placehold.co/400x400',
+			price: 200,
+			quantity: 200,
+			category: 'monitors',
+		};
+
+		amount += 1;
+
+		try {
+			const response = await fetch('/api/products', {
+				body: JSON.stringify(mockdata),
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+			});
+
+			if (response.ok) {
+				console.log('Added');
+			}
+		} catch (e) {
+			console.error(e);
+		}
+	}
+</script>
+
+<button
+	onclick={async () => await onclick()}
+	class="cursor-pointer">add</button
+>
