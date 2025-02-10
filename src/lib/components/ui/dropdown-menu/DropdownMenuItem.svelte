2ea159ff56732ff3bd9165a228643afba5d52ef5
Falk33n <tim.falk00@gmail.com>
2025-02-10 07:04:03 +0100

    feat: add dropdown

---
+++ src/lib/components/ui/dropdown-menu/DropdownMenuItem.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { cn } from '$lib/utils';
+	import { DropdownMenu as DropdownMenuPrimitive } from 'bits-ui';
+
+	type Props = DropdownMenuPrimitive.ItemProps & {
+		inset?: boolean;
+	};
+
+	let {
+		ref = $bindable(null),
+		class: className,
+		inset,
+		...props
+	}: Props = $props();
+</script>
+
+<DropdownMenuPrimitive.Item
+	bind:ref
+	class={cn(
+		'data-[highlighted]:bg-accent data-[highlighted]:text-accent-foreground relative flex cursor-default items-center gap-2 rounded-sm px-2 py-1.5 text-sm outline-hidden transition-colors select-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50 [&>svg]:size-4 [&>svg]:shrink-0',
+		inset && 'pl-8',
+		className,
+	)}
+	{...props}
+/>
---
+++ src/lib/components/ui/dropdown-menu/DropdownMenuShortcut.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { cn } from '$lib/utils';
+	import { type WithElementRef } from 'bits-ui';
+	import type { HTMLAttributes } from 'svelte/elements';
+
+	type Props = WithElementRef<HTMLAttributes<HTMLSpanElement>>;
+
+	let {
+		ref = $bindable(null),
+		class: className,
+		children,
+		...props
+	}: Props = $props();
+</script>
+
+<span
+	bind:this={ref}
+	class={cn('ml-auto text-xs tracking-widest opacity-60', className)}
+	{...props}
+>
+	{@render children?.()}
+</span>
