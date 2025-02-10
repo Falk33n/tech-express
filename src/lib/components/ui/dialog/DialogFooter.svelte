63d7e7364eaf17062def08c1eefbe9365b51f7d1
Falk33n <tim.falk00@gmail.com>
2025-02-10 07:04:22 +0100

    feat: add dialog

---
+++ src/lib/components/ui/dialog/DialogFooter.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { cn } from '$lib/utils';
+	import type { WithElementRef } from 'bits-ui';
+	import type { HTMLAttributes } from 'svelte/elements';
+
+	type Props = WithElementRef<HTMLAttributes<HTMLDivElement>>;
+
+	let {
+		ref = $bindable(null),
+		class: className,
+		children,
+		...props
+	}: Props = $props();
+</script>
+
+<div
+	bind:this={ref}
+	class={cn(
+		'flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2',
+		className,
+	)}
+	{...props}
+>
+	{@render children?.()}
+</div>
