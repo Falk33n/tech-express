cbaf2500acc9abc984d5e781cac58b818c9f3802
Falk33n <tim.falk00@gmail.com>
2025-02-11 15:48:16 +0100

    feat: add icons

---
+++ src/lib/components/ui/icons/LockIcon.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import type { IconProps } from '$lib/components/ui/icons';
+	import { cn } from '$lib/utils';
+
+	let { ref = $bindable(), class: className, ...props }: IconProps = $props();
+</script>
+
+<svg
+	bind:this={ref}
+	class={cn('scale-105', className)}
+	xmlns="http://www.w3.org/2000/svg"
+	width="24"
+	height="24"
+	viewBox="0 0 24 24"
+	fill="none"
+	stroke="currentColor"
+	stroke-width="2"
+	stroke-linecap="round"
+	stroke-linejoin="round"
+	{...props}
+>
+	<rect
+		width="18"
+		height="11"
+		x="3"
+		y="11"
+		rx="2"
+		ry="2"
+	/>
+	<path d="M7 11V7a5 5 0 0 1 10 0v4" />
+</svg>
