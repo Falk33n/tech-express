c92d8a81e8c0da656617e5c4a70e2f3492408577
Falk33n <tim.falk00@gmail.com>
2025-02-25 09:00:49 +0100

    feat: add table

---
+++ src/lib/components/ui/table/Table.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { cn } from '$lib/utils';
+	import type { WithElementRef } from 'bits-ui';
+	import type { HTMLTableAttributes } from 'svelte/elements';
+
+	type Props = WithElementRef<HTMLTableAttributes>;
+
+	let {
+		ref = $bindable(null),
+		class: className,
+		children,
+		...props
+	}: Props = $props();
+</script>
+
+<div class="relative w-full overflow-auto">
+	<table
+		bind:this={ref}
+		class={cn('w-full caption-bottom text-sm', className)}
+		{...props}
+	>
+		{@render children?.()}
+	</table>
+</div>
---
+++ src/lib/components/ui/table/TableCaption.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { cn } from '$lib/utils';
+	import type { WithElementRef } from 'bits-ui';
+	import type { HTMLAttributes } from 'svelte/elements';
+
+	type Props = WithElementRef<HTMLAttributes<HTMLElement>>;
+
+	let {
+		ref = $bindable(null),
+		class: className,
+		children,
+		...props
+	}: Props = $props();
+</script>
+
+<caption
+	bind:this={ref}
+	class={cn('text-muted-foreground mt-4 text-sm', className)}
+	{...props}
+>
+	{@render children?.()}
+</caption>
