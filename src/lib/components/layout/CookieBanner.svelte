be35c6a9d7e5d0ed60bb587145316f415ec0bfc6
Falk33n <tim.falk00@gmail.com>
2025-03-13 13:38:40 +0100

    feat: add cookiebanner

---
+++ src/lib/components/layout/CookieBanner.svelte
@@ -0,0 +1,47 @@
+<script>
+	import { Button } from '$lib/components/ui/button';
+	import {
+		Card,
+		CardContent,
+		CardDescription,
+		CardHeader,
+		CardTitle,
+	} from '$lib/components/ui/card';
+	import { XIcon } from 'lucide-svelte';
+
+	let isClosed = $state(false);
+</script>
+
+{#if !isClosed}
+	<Card
+		class="fixed bottom-0 left-0 z-[1000] w-full md:bottom-8 md:left-8 md:w-[550px]"
+	>
+		<CardHeader>
+			<CardTitle class="relative mb-4">
+				Manage Cookie Consent
+
+				<Button
+					variant="ghost"
+					size="icon"
+					class="absolute top-1/2 -right-2 -translate-y-1/2"
+					onclick={() => (isClosed = true)}
+					aria-label="Close Cookie Banner"
+				>
+					<XIcon
+						aria-hidden
+						class="size-4 scale-125"
+					/>
+				</Button>
+			</CardTitle>
+
+			<CardDescription>
+				To provide the best experiences, we use technologies like cookies to
+				store and/or access device information.
+			</CardDescription>
+		</CardHeader>
+
+		<CardContent>
+			<Button class="w-full">Accept</Button>
+		</CardContent>
+	</Card>
+{/if}
--- src/lib/components/layout/index.ts
+++ src/lib/components/layout/index.ts
@@ -1,3 +1,4 @@
+export { default as CookieBanner } from './CookieBanner.svelte';
 export { default as Footer } from './Footer.svelte';
 export { default as Navbar } from './Navbar.svelte';
 export { default as Newsletter } from './Newsletter.svelte';
